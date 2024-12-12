7a6bedb595294922a54e057cbcedef71c3644a33
uhyeonkim96 <uhkim96@naver.com>
2024-12-12 13:55:02 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String id= sc.next();
+        if(id.equals(id)){
+            System.out.println(id+"??!");
+        }
+    }
+}
