ba381857bad310df27cfbb1cb29c7f118c2ebbcb
uhyeonkim96 <uhkim96@naver.com>
2025-05-01 11:19:41 +0900

    [Bronze III] Title: 직각삼각형, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,27 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while(true){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
 
-            if(a == 0 && b ==0 && c ==0){
-                break;
+            int a = Integer.parseInt(tokens[0]);
+            int b = Integer.parseInt(tokens[1]);
+            int c = Integer.parseInt(tokens[2]);
+
+            if(a==0 && b==0 && c==0) break;
+
+            int[] sides = {a,b,c};
+            Arrays.sort(sides);
+
+            if(sides[0]*sides[0] + sides[1]*sides[1] == sides[2]*sides[2]){
+                System.out.println("right");
+            }else{
+                System.out.println("wrong");
             }
-
-        int[] sides = {a, b, c};
-        java.util.Arrays.sort(sides);
-
-        if(sides[0]*sides[0]+sides[1]*sides[1]==sides[2]*sides[2]){
-            System.out.println("right");
-        }else{
-            System.out.println("wrong");
         }
-        }
-        sc.close();
-
     }
 }
